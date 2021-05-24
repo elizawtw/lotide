eb382c6649a8122cf99cd86c310186851b2854d6
elizawtw <twew02@gmail.com>
2021-05-24 19:59:19 +0000

    created head function

---
+++ head.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion failed: ${actual} !== ${expected}`);
+  }
+   
+};
+
+const head = function(actual){
+  for (let i = 0; i < actual.length; i++) {
+    return actual[0];
+  }
+
+}
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(10, 6);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), "Hello");
