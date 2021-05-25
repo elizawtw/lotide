700a298548403bb6817be4ff008c8e893e9babee
elizawtw <twew02@gmail.com>
2021-05-25 20:11:27 +0000

    created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(first, second){
+  let check = true;
+  //check length of arrays
+  if (first.length !== second.length) {
+    check = false;
+  }
+  //if length is same
+  for (let i = 0; i < first.length; i++){
+    if (first[i] !== second[i]){
+      check = false;
+    }
+  }
+  return check;
+}
+
+const assertArraysEqual = function(first, second) {
+  if(eqArrays(first, second)){
+    console.log(`✅✅✅Assertion passed: ${first} === ${second}`);
+  } else {
+    console.log(`❌❌❌Assertion failed: ${first} !== ${second}`);
+}
+}
+
+assertArraysEqual([1,2,3],[1,2,4])
