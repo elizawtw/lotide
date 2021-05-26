829de6781d717db537237f4ed1911b151a5e4373
elizawtw <twew02@gmail.com>
2021-05-26 17:39:35 +0000

    eslinted

--- countOnly.js
+++ countOnly.js
@@ -11,19 +11,19 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-   if(itemsToCount[item]){
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
-    }
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
 
-   } 
+    }
     
     
   }
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
