0fa8c920897205174d104b7dd1ee7d21bab62ad9
elizawtw <twew02@gmail.com>
2021-05-31 17:32:53 +0000

    object shorthand for index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const list = {
+  head: require('./head'),
+  tail: require('./tail'),
+  middle: require('./middle'),
+  assertEqual: require('./assertEqual'),
+  assertArraysEqual: require('./assertArraysEqual'),
+  eqArrays : require('./eqArrays')
+}
+
+module.exports = list;
